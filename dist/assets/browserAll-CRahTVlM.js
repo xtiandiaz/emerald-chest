3721fb5fdc887f9c3bf684cd4e60412fc58f8936
Cristian Díaz <xtndiaz@gmail.com>
2026-01-26 12:57:57 +0200

    add dist folder

---
+++ dist/assets/browserAll-CRahTVlM.js
@@ -0,0 +1,2 @@
+import{e as a,A as e,C as i,a as s,E as t,F as n,D as d}from"./index-DlakcZb0.js";import"./webworkerAll-D_wO675K.js";a.add(e);a.mixin(i,s);a.add(t);a.mixin(i,n);a.add(d);
+//# sourceMappingURL=browserAll-CRahTVlM.js.map
